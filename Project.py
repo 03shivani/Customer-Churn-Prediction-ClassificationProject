4a6a4fba3d3a8f5d9d312ffb925031b7504d0e7f
Shivani bhavsar <101508448+03shivani@users.noreply.github.com>
2023-02-12 16:37:49 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as plt
-import seaborn as sns
-
 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
